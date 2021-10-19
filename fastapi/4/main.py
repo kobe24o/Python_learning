2a56c24d491f680922de7b2a3530f2db9d3ead2a
kobe24o <mingmingchen1989@163.com>
2021-10-19 09:29:21 +0800

    body

--- fastapi/4/main.py
+++ fastapi/4/main.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI, Path, Body
-from typing import Optional, List
-from pydantic import BaseModel, Field
+from typing import Optional, List, Set, Dict
+from pydantic import BaseModel, Field, HttpUrl
 app = FastAPI()
 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 
 class Item(BaseModel):
     name: str
     price: float = Field(..., gt=0, description="price must be greater than 0")
     description: Optional[str] = Field(None, title="description of item", max_length=30)
     tax: Optional[float] = None
-    tags: List[str] = []
+    tags: Set[str] = []
+    image: Optional[List[Image]] = None
+    
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item = Body(..., embed=True)):
     res = {"item_id" : item_id, "item" : item}
     return res
+@app.post("/images/multiple/")
+async def create_multiple_images(images: List[Image]):
+    return images
+@app.post("/index-weights/")
+async def create_index_weights(weights: Dict[int, float]):
+    return weights
