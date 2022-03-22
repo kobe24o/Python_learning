6190a879adeb5b793b00539c2cab191701a317e1
kobe24o <chenmingming@huohua.cn>
2022-03-22 09:28:20 +0800

    tortoise_orm

--- web_python_dev/sql_tortoise_orm/models.py
+++ web_python_dev/sql_tortoise_orm/models.py
@@ -5,15 +5,15 @@
 # @desc :
 
 from datetime import datetime
-from typing import Optional
-from pydantic import BaseModel, Field
+from typing import Optional, List
+from pydantic import BaseModel, Field, validator
 from tortoise.models import Model
 from tortoise import fields
 
 class PostBase(BaseModel):
     title: str
     content: str
-    created_at: datetime = Field(default_factory=datetime.now)
+    publication_date: datetime = Field(default_factory=datetime.now)
     class Config:
         orm_mode = True
 
@@ -34,3 +34,40 @@ class PostTortoise(Model):
     content = fields.TextField(null=False)
     class Meta:
         table = 'posts'
+
+
+# 评论类
+class CommentBase(BaseModel):
+    post_id: int
+    publication_date: datetime = Field(default_factory=datetime.now)
+    content: str
+
+    class Config:
+        orm_mode = True
+
+
+class CommentCreate(CommentBase):
+    pass
+
+
+class CommentDB(CommentBase):
+    id: int
+
+class PostPublic(PostDB):
+    comments: List[CommentDB]
+
+    @validator("comments", pre=True)
+    def fetch_comments(cls, v):
+        return list(v)
+
+
+class CommentTortoise(Model):
+    id = fields.IntField(pk=True, generated=True)
+    post = fields.ForeignKeyField(
+        "models.PostTortoise", related_name="comments", null=False
+    )
+    publication_date = fields.DatetimeField(null=False)
+    content = fields.TextField(null=False)
+
+    class Meta:
+        table = "comments"
