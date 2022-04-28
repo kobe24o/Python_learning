ecfb0426f086c932a6eecdb24aa573335ae33235
kobe24o <chenmingming@huohua.cn>
2022-04-28 16:02:23 +0800

    pyqt

--- qt/ch1/childform2.py
+++ qt/ch1/childform2.py
@@ -1,32 +1,20 @@
 # -*- coding: utf-8 -*-
+# childform2.py
 
-"""
-Module implementing MainWindow_child.
-"""
 import sys
-
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QFileDialog
-
 from Ui_childform2 import ui_childform
 from Ui_menu_bar_tool_bar import Ui_MainWindow
 
 
 class MainWindow_withchild(QMainWindow, Ui_MainWindow):
-    """
-    Class documentation goes here.
-    """
+
     def __init__(self, parent=None):
-        """
-        Constructor
-        
-        @param parent reference to the parent widget (defaults to None)
-        @type QWidget (optional)
-        """
         super(MainWindow_withchild, self).__init__(parent)
         self.setupUi(self)
 
-        self.child = my_childform() # 创建子窗口实例
+        self.child = my_childform()  # 创建子窗口实例
         self.actionclose.triggered.connect(self.close)
         self.actionopen_O.triggered.connect(self.openMsg)
         self.addWinAction.triggered.connect(self.childshow)
@@ -40,10 +28,12 @@ class MainWindow_withchild(QMainWindow, Ui_MainWindow):
         # 在状态栏显示文件地址
         self.statusbar.showMessage(file)
 
+
 class my_childform(QWidget, ui_childform):
     def __init__(self):
         super(my_childform, self).__init__()
         self.setupUi(self)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
